61320c532bfd8827dc004bb86d21d24b0eefbbc3
amandigital092-art <aman.digital092@gmail.com>
2026-03-25 14:09:30 +0530

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://cannabisdeal.online/");
+  res.setHeader("Access-Control-Allow-Origin", "https://cannabisdeal.online");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
